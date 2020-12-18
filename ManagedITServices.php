8840bd6156b82ebd78f1dcb5cd3da09392642f3a
Nahumkelly <46894120+Nahumkelly@users.noreply.github.com>
2020-12-18 16:27:32 -0500

    ,

--- ManagedITServices.php
+++ ManagedITServices.php
@@ -1,6 +1,6 @@
 <?php
-$title = "Managed IT Services";
-$page = "dropdown";
+$title = "About Us";
+$page = "aboutus";
 require_once "includes/header.php";
 require_once "includes/banner.php";
 require_once 'db/conn.php';
@@ -8,117 +8,79 @@ require_once 'db/conn.php';
 //get all Gender
 $result = $crud->getGender();
 ?>
-        <div>
-            <div class="row">
-                <div class="col">
-                    <h2 class="text-center text-primary shadow-sm"><br><strong>Managed IT Services for Stable and Reliably Evolving IT Environment</strong>
-                    <br>
-                    <img src="img/ManagedIT.png"><br></h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <p class="lead text-left">Managed IT services are aimed at handing over your IT operations to a third-party IT services provider, which takes responsibility to ensure high efficiency and timely improvement of your IT environment and processes within it.<br></p>
-                    <p
-                        class="lead text-left"><strong>Managed IT services by Kelly's Computer Service backed with 12 years of experience in ITSM</strong>&nbsp;span the&nbsp;<strong>all-round support for cloud, on-premises and hybrid infrastructures</strong>&nbsp;and include consulting
-                        and migration services for proactive IT evolution.<br></p>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col">
-                    <div class="card shadow-lg">
-                        <div class="card-body">
-                            <h4 class="card-title"><br><strong>IT Stability without Excessive Spending? Possible!</strong><br><br></h4>
-                            <p class="lead card-text">Kelly’s Computer Service professionals provide proactive IT infrastructure monitoring, regular software configuration and updates, resource usage optimization, user support and overall improvement of your IT infrastructure
-                                and its components.<br><br></p>
-                        </div>
+<div class="row">
+    <div class="col">
+        <h2 class="text-center" style="color: rgb(0,123,255);"><br><strong>About Kelly's Computer Service – Global IT Company</strong><br><br></h2>
+    </div>
+</div>
+<div class="row">
+    <div class="col-10 offset-1">
+        <p class="text-left" style="font-size: 20px;"><br>Founded in 2017, Kelly' Computer Service is a provider of&nbsp;<strong>IT consulting</strong>&nbsp;and&nbsp;<strong>software development services</strong>. Having started as a small AI product company, we switched to IT services in 2002
+            and ever since we have helped non-IT organizations and software product companies improve business performance and quickly win new customers.<br><br></p>
+            <img class="rounded-circle img-fluid shadow-lg" data-bs-hover-animate="swing" src="img/homepageimage.jpg" width="1000px" height="500ox">
+    </div>
+</div>
+<div class="row">
+    <div class="col"></div>
+</div>
+</div>
+</div>
+<div class="projects-horizontal">
+    <div class="container">
+        <div class="intro">
+            <h2 class="text-center" style="color: rgb(0,123,255);">Meet the Management Team</h2>
+        </div>
+        <div class="row projects">
+            <div class="col-sm-6 item">
+                <div class="row">
+                    <div class="col-md-12 col-lg-5">
+                        <a href="#"><img class="rounded-circle img-fluid shadow-lg" src="img/kelly.png"></a>
+                    </div>
+                    <div class="col">
+                        <h3 class="name">Nahum Kelly</h3>
+                        <p class="description">CEO, Kelly's Computer Service<br><br>Mr. Kelly has been leading our company for 5 years. He holds a Bachelor Degree's in Information Communication Technology From&nbsp;&nbsp;<br>Vocational Training Development Institute&nbsp;&nbsp;Under
+                            his leadership, Kelly's Computer Service&nbsp; achieved 3 times growth.<br><br></p>
                     </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="col">
-                    <h2 class="text-center text-primary shadow-sm"><strong>IT INFRASTRUCTURE COMPONENTS OUR MANAGED IT SERVICES TACKLE</strong><br><br></h2>
-                    <div class="row"></div>
-                    <div class="row">
-                        <div class="col">
-                            <ul>
-                                <li class="d-block" style="font-size: 24px;">Networks.</li>
-                                <li class="d-block" style="font-size: 24px;">On-premises data centers.</li>
-                                <li class="d-block" style="font-size: 24px;">Cloud services (IaaS, PaaS, SaaS).</li>
-                                <li class="d-block" style="font-size: 24px;">Cybersecurity tools.</li>
-                                <li class="d-block" style="font-size: 24px;">Databases, data warehouses, data lakes.</li>
-                            </ul>
-                        </div>
-                        <div class="col">
-                            <ul>
-                                <li class="d-block" style="font-size: 24px;">Applications and app infrastructures.</li>
-                                <li class="d-block" style="font-size: 24px;">Development infrastructures (CI/CD, development and testing environments).</li>
-                                <li class="d-block" style="font-size: 24px;">Websites and web portals.</li>
-                                <li class="d-block" style="font-size: 24px;">Desktops.</li>
-                            </ul>
-                        </div>
+            <div class="col-sm-6 item">
+                <div class="row">
+                    <div class="col-md-12 col-lg-5">
+                        <a href="#"><img class="rounded-circle img-fluid shadow-lg" src="img/gillian.jpg" width="200px" height="100px"></a>
                     </div>
-                    <div class="row">
-                        <div class="col">
-                            <h3 class="text-center text-primary shadow-sm" >
-                        <img class="rounded img-fluid" src="img/networkinfer.png">
-                        </h3>
-                        </div>
+                    <div class="col">
+                        <h3 class="name">Gillian Wallace</h3>
+                        <p class="description">VP Sales and BD&nbsp;Kelly's Computer Service<br>Miss. Wallace has been Kelly’s Computer Service VP for 12 years. She holds a PhD in Management from UWI Erasmus University. She is an author of a number of academic and business
+                            papers on IT outsourcing, online services procurement and online IT marketplaces.<br></p>
                     </div>
-                    <br/>
-                    <div class="row">
-                        <div class="col">
-                            <div class="card text-light bg-primary text-left border rounded-0 shadow" style="font-size: 18px;text-align: left;">
-                                <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;background: #fbf8f8;color: rgb(3,126,249);"><strong>Basic</strong><br></h4>
-                                    <p>Get your infrastructure&nbsp;<strong>continuously supported and optimized</strong>:<br></p>
-                                    <ul>
-                                        <li>Proactive infrastructure monitoring</li>
-                                        <li>IT infrastructure administration:</li>
-                                        <li>User administration</li>
-                                        <li>Regular software configuration and updates</li>
-                                        <li>Network, data center, cloud service management</li>
-                                        <li>L1, L2, L3 support</li>
-                                        <li>Cloud usage optimization</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <br/>
-                        <div class="col">
-                            <div class="card text-light bg-primary text-left border rounded-0 shadow" style="font-size: 18px;text-align: left;">
-                                <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;background: #fbf8f8;color: rgb(3,126,249);"><strong>Extended</strong><br></h4>
-                                    <p>Includes the&nbsp;<strong>Basic package and&nbsp;consulting on new infrastructure initiatives:</strong><br></p>
-                                    <ul>
-                                        <li>Analyzing problems in IT infrastructure and providing recommendations</li>
-                                        <li>Designing a new IT infrastructure or its components</li>
-                                        <li>Designing CI/CD pipelines</li>
-                                        <li>Advising on security improvement using the SecOps approach</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <br/>
-                        <div class="col">
-                            <div class="card text-light bg-primary text-left border rounded-0 shadow" style="font-size: 18px;text-align: left;">
-                                <div class="card-body">
-                                    <h4 class="card-title" style="text-align: center;background: #fbf8f8;color: rgb(3,126,249);"><strong>Extended Plus</strong><br></h4>
-                                    <p>Includes the&nbsp;<strong>Extended package and modernization of your IT</strong>&nbsp;infrastructure:<br></p>
-                                    <ul>
-                                        <li>Application and data warehouse migration to cloud</li>
-                                        <li>Integration of cloud and on-premises apps</li>
-                                        <li>Infrastructure and app evolution:</li>
-                                        <li>Introduction of minor fixes and major updates</li>
-                                        <li>App delivery automaion</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
+                </div>
+            </div>
+            <div class="col-sm-6 item">
+                <div class="row">
+                    <div class="col-md-12 col-lg-5">
+                        <a href="#"><img class="rounded-circle img-fluid shadow-lg" src="img/yanick.jpg" width="200px"></a>
+                    </div>
+                    <div class="col">
+                        <h3 class="name">Yanick Levy</h3>
+                        <p class="description">Chief Technology Officer<br>Mr. Levy entered Kelly's Computer Service&nbsp; in Late 2017 and became CTO in 2018. He holds a&nbsp;<br>Bachelor Degree's in Information Communication Technology From&nbsp;&nbsp;<br>Vocational Training
+                            Development Institute&nbsp;. Under his management, we launched Mobile, SharePoint and CRM development services and grew to 700 IT experts.<br></p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 item">
+                <div class="row">
+                    <div class="col-md-12 col-lg-5">
+                        <a href="#"><img class="rounded-circle img-fluid shadow-lg" src="img/Rev.jpg"></a>
+                    </div>
+                    <div class="col">
+                        <h3 class="name">Revonda McLeod&nbsp;</h3>
+                        <p class="description">CEO, Kelly's Computer Service<br><br>With a Bachelor’s degree in&nbsp;<br><strong>Logistics management</strong>&nbsp;, entered Kelly's Computer Service in 2017 and worked with us for two years in different positions from software
+                            development to project management.&nbsp; Miss. McLeod is a Microsoft Certified Professional and Microsoft Certified Technology Specialist, and she also holds the Professional Scrum Developer certificate.<br><br></p>
                     </div>
                 </div>
             </div>
         </div>
-        <br>
     </div>
-    <?php require_once "includes/footer.php"; ?>
+</div>
+
+<?php require_once "includes/footer.php"; ?>
